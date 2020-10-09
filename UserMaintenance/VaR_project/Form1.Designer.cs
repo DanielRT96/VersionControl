890ac73b1ffe79fd8e600aebb77da36078f6d605
Dániel Réti <daniel.reti@stud.uni-corvinus.hu>
2020-10-09 17:57:26 +0200

    minor fixes

--- UserMaintenance/VaR_project/Form1.Designer.cs
+++ UserMaintenance/VaR_project/Form1.Designer.cs
@@ -53,11 +53,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(494, 301);
+            this.button1.Location = new System.Drawing.Point(551, 326);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(116, 50);
             this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
+            this.button1.Text = "Save into .csv";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
