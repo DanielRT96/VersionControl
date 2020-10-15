02894a0f704e547310deffd303ec639ce18e03ae
Dániel Réti <daniel.reti@stud.uni-corvinus.hu>
2020-10-15 13:25:34 +0200

    dynamic function

--- UserMaintenance/webService/Form1.cs
+++ UserMaintenance/webService/Form1.cs
@@ -21,8 +21,7 @@ namespace webService
         public Form1()
         {
             InitializeComponent();
-            GetExchangeRates();
-            dataGridView1.DataSource = Rates;
+            RefreshData();
         }
 
         private void GetExchangeRates()
@@ -31,9 +30,9 @@ namespace webService
 
             var request = new GetExchangeRatesRequestBody()
             {
-                currencyNames = "EUR",
-                startDate = "2020-01-01",
-                endDate = "2020-06-30"
+                currencyNames = (comboBox1.SelectedItem).ToString(),
+                startDate = (dateTimePicker1.Value).ToString(),
+                endDate = (dateTimePicker2.Value).ToString()
             };
 
             var response = mnbService.GetExchangeRates(request);
@@ -76,6 +75,28 @@ namespace webService
             chartArea.AxisY.MajorGrid.Enabled = false;
             chartArea.AxisY.IsStartedFromZero = false;
 
+        }
+
+        private void RefreshData()
+        {
+            Rates.Clear();
+            dataGridView1.DataSource = Rates;
+            GetExchangeRates();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshData();
         }
     }
 
