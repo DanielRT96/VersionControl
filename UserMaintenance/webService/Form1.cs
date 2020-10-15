609604f2e4d36e41f877a4b3285fc735a5e193c6
Dániel Réti <daniel.reti@stud.uni-corvinus.hu>
2020-10-15 13:08:43 +0200

    add chart

--- UserMaintenance/webService/Form1.cs
+++ UserMaintenance/webService/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 using webService.Entities;
 using webService.MnbServiceReference;
@@ -59,6 +60,21 @@ namespace webService
 
             }
 
+            chartRateData.DataSource = Rates;
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
 
         }
     }
