c64ffbb5a188d2de80dbcccce533cb19935609db
Dániel Réti <daniel.reti@stud.uni-corvinus.hu>
2020-11-11 22:14:56 +0100

    color change

--- UserMaintenance/Factory/Form1.cs
+++ UserMaintenance/Factory/Form1.cs
@@ -81,9 +81,21 @@ namespace Factory
 
         private void button2_Click(object sender, EventArgs e)
         {
-            Factory = new BallFactory();
+            Factory = new BallFactory
+            {
+                BallColor = button3.BackColor
+            };
         }
 
-        
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            var colorPicker = new ColorDialog();
+
+            colorPicker.Color = button.BackColor;
+            if (colorPicker.ShowDialog() != DialogResult.OK)
+                return;
+            button.BackColor = colorPicker.Color;
+        }
     }
 }
