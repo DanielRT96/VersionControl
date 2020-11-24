70fc00e11756b88c58a3533973b6f2daafb31984
Dániel Réti <daniel.reti@stud.uni-corvinus.hu>
2020-11-24 19:33:27 +0100

    choose winner

--- UserMaintenance/EvolutionAlgorithm/Form1.cs
+++ UserMaintenance/EvolutionAlgorithm/Form1.cs
@@ -20,6 +20,9 @@ namespace EvolutionAlgorithm
         int nbrOfSteps = 10;
         int nbrOfStepsIncrement = 10;
         int generation = 1;
+
+        Brain winnerBrain = null;
+
         public Form1()
         {
             InitializeComponent();
@@ -47,6 +50,17 @@ namespace EvolutionAlgorithm
                              orderby p.GetFitness() descending
                              select p;
             var topPerformers = playerList.Take(populationSize / 2).ToList();
+
+            var winners = from p in topPerformers
+                          where p.IsWinner
+                          select p;
+            if (winners.Count() > 0)
+            {
+                winnerBrain = winners.FirstOrDefault().Brain.Clone();
+                gc.GameOver -= Gc_GameOver;
+                return;
+            }
+
 
             gc.ResetCurrentLevel();
             foreach (var p in topPerformers)
